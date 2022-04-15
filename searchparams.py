10c75197c24ad883506ee95eb35be8a1d423d675
pixls <tha.pixls@gmail.com>
2022-04-15 18:14:08 -0400

    started setting up app env and structure
    todo list in readme incomplete

--- searchparams.py
+++ searchparams.py
@@ -1,3 +1,5 @@
+
+
 class YTSearch:
     params = {
         "videoCategoryId":"10",
@@ -5,3 +7,11 @@ class YTSearch:
         "type":"video"
         }
     root_url = "https://www.googleapis.com/youtube/v3/search"
+
+
+class LastFMSearch:
+    params = {
+        "method":"track.search",
+        "format":"json"
+    }
+    root_url = "http://ws.audioscrobbler.com/2.0/"
