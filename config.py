f9239f8fca3751bcc38aedf6620f1361d4d6099a
pixls <tha.pixls@gmail.com>
2022-04-18 13:28:59 -0400

    setting up environment further
    basic root view created just for testing purposes

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """
+    Base configuration class. Contains default config settings.
+    """
+
+    SQLALCHEMY_DATABASE_URI = "postgresql:///cots"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    FLASK_ENV = "development"
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY", default="DEFAULT SECRET KEY")
+    LASTFM_KEY = os.getenv("LASTFM_KEY", default="")
+    LASTFM_SECRET = os.getenv("LASTFM_SECRET", default="")
+    YOUTUBE_KEY = os.getenv("YOUTUBE_KEY", default="")
+
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = os.getenv("SECRET_KEY", default="DEV SECRET KEY")
+    TESTING = False
+
+
+class TestingConfig(Config):
+    WTF_CSRF_ENABLED = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://"
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    DEBUG_TB_ENABLED = False
+    SECRET_KEY = "TEST SECRET KEY"
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = "production"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "PROD_DATABASE_URL", default="postgresql:///cots"
+    )
