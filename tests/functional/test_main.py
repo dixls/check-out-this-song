4de932a7204f3040dfe6019f5f2d6512a0aad1b8
pixls <tha.pixls@gmail.com>
2022-05-04 15:42:22 -0400

    adding signup tests

--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 
-def test_root(test_app):
+def test_root(test_app, test_db):
 
     with test_app.test_client() as test_client:
         response = test_client.get("/")
@@ -46,3 +46,47 @@ def test_login_post_bad_username(test_app, persisted_user):
             follow_redirects=True,
         )
         assert b"Username not found" in response.data
+
+
+def test_signup_get(test_app):
+
+    with test_app.test_client() as test_client:
+        response = test_client.get("/signup")
+        assert response.status_code == HTTPStatus.OK
+        assert b"Sign up for Check Out This Song!" in response.data
+
+
+def test_signup_post(test_app, test_db):
+
+    with test_app.test_client() as test_client:
+        response = test_client.post(
+            "/signup",
+            data={
+                "username": "testSignupUser",
+                "password": "test_signup_password",
+                "email": "test_email@email.com",
+                "avatar": None,
+                "bio": "",
+            },
+            follow_redirects=True,
+        )
+        print(response.data)
+        assert b"Welcome to Check Out This Song" in response.data
+
+
+def test_signup_post_illegal_username(test_app, test_db):
+
+    with test_app.test_client() as test_client:
+        response = test_client.post(
+            "/signup",
+            data={
+                "username": "test_signup_user",
+                "password": "test_signup_password",
+                "email": "test_email@email.com",
+                "avatar": None,
+                "bio": "",
+            },
+            follow_redirects=True,
+        )
+        print(response.data)
+        assert b'<p class="help is-danger">' in response.data
